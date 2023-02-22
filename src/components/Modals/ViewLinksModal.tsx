2e5186932d03211775aa5663d097fdcc0fe0d448
one-moonman <nicola.manoilov@gmail.com>
2023-02-22 11:49:08 +0200

    changed name display in modal

--- src/components/Modals/ViewLinksModal.tsx
+++ src/components/Modals/ViewLinksModal.tsx
@@ -31,7 +31,7 @@ export default function QrModal({ dataUrl, name, url }: TProps) {
             <Modal.Header />
             <Modal.Body className="flex flex-col items-center pt-10">
                 <Image alt="qr" src={dataUrl} className="w-full" width={200} height={200} />
-                <h1 className="font-bold text-xl mt-5">{name}</h1>
+                <h1 className="font-bold text-xl mt-5">{name.replace('_', " ")}</h1>
                 <Button color="gray" className="w-full mt-5 mb-2" onClick={() => copy(dataUrl)}>
                     <MdOutlineQrCode2 className="mr-5" />Copy QR</Button>
                 <Button gradientDuoTone="cyanToBlue" className="w-full" onClick={() => copy(url)}>
